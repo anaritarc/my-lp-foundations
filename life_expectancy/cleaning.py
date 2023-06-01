211d9f6467e4e3992918619d529ba7835515ba96
Rita Carvalho <ritacarvalho@ritas-mbp.lan>
2023-06-01 17:34:46 +0100

    pylint resolution

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -10,12 +10,15 @@ def clean_data(data: pd.DataFrame, region: str) -> pd.DataFrame:
         data = data.melt(id_vars = descrip_column)
         data[['unit','sex', 'age', 'region']] = data[descrip_column].str.split(',', expand=True)
 
-        data['value'] = data['value'].str.extractall(r'(\d+.\d+)').astype(float).unstack().max(axis=1)
+        data['value'] = (data['value'].
+                         str.extractall(r'(\d+.\d+)').
+                         astype(float)
+                         .unstack().max(axis=1))
         data['value'] = pd.to_numeric(data['value'], errors='coerce')
         data['year'] = data['variable'].astype(int)
 
         data = data.dropna(axis = 0)
-    
+
     data = data[['unit', 'sex', 'age', 'region', 'year', 'value']]
 
     data_by_region = data[data.region.str.lower() == region.lower()]
